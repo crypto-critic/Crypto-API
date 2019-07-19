73e47369b61cde942f57a5818e7431e56a33f570
pinokara <hec.stw@gmail.com>
2019-07-19 08:05:50 +0700

    get Listcoin of vps

--- global/getList.js
+++ global/getList.js
@@ -1,17 +1,9 @@
 const List = require('./models/ListCoin');
-const fs = require('fs');
-const path = require('path');
-const indexPath = fs.realpathSync(process.cwd());
+const Coin = require('../coin/coin');
 
-const getList = async ()=>{
-    let ListOfCoin = await List.find({});
-    let ListCoin = ListOfCoin.map(coin => {
-        let destination = path.resolve(indexPath, `initial/${coin.coinId}.json`);
-        let rawData = fs.readFileSync(destination);
-        let data = JSON.parse(rawData);
-        return data;
-    });
+const getList = async () => {
+    data = await List.find({});
+    let ListCoin = await data.map(data => new Coin(data));
     return ListCoin;
-};
-
+}
 module.exports = getList;
