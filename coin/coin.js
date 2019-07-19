3902deb1c9759706efbfbc55b977d683b1145dfa
pinokara <hec.stw@gmail.com>
2019-07-19 08:03:33 +0700

    add market props to coin object

--- coin/coin.js
+++ coin/coin.js
@@ -8,12 +8,14 @@ const Rich = require('./blockchain-models/rich');
 const TX = require('./blockchain-models/tx');
 const UTXO = require('./blockchain-models/utxo');
 const BlockReward = require('./blockchain-models/blockreward');
+const Market = require('./market-models/market/market');
 const Router = require('./router/createRouter');
+const getDataFromCoingecko = require('../lib/getDataFromCoingecko');
 
 class Coin {
     constructor(object){
-        this.id = object.id;
-        this.connection = new Connection(object.id).connection;
+        this.coinId = object.coinId;
+        this.connection = new Connection(object.coinId).connection;
         this.rpc = new RPC(object.wallet);
         this.block = new Block(this.connection).model;
         this.coin = new CoinX(this.connection).model;
@@ -23,12 +25,8 @@ class Coin {
         this.tx = new TX(this.connection).model;
         this.utxo = new UTXO(this.connection).model;
         this.blockreward = new BlockReward(this.connection).model;
+        this.market = new Market(this.connection).model;
         this.router = new Router(this).router;
-        // this.marketData = {};
-        // this.syncMarketData();
-    }
-    syncMarketData(){
-        console.log('sync market data')
     }
 }
 module.exports = Coin;
