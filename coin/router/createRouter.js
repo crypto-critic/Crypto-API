4d0c80007b571e3af1afe789f95503aa2c7322fa
pinokara <hec.stw@gmail.com>
2019-07-20 00:53:22 +0700

    Blocktime will have another router

--- coin/router/createRouter.js
+++ coin/router/createRouter.js
@@ -8,7 +8,7 @@ class Router {
         const market = require('./marketRouter')(coin);
         const router = express.Router();
         router.get('/address/:hash', blockex.getAddress);
-        router.get('/block/average', blockex.getAvgBlockTime);
+        // router.get('/block/average', blockex.getAvgBlockTime);
         router.get('/block/is/:hash', blockex.getIsBlock);
         router.get('/block/:hash', blockex.getBlock);
         router.get('/coin', blockex.getCoin);
