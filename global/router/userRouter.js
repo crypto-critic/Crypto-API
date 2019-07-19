37522756cecef29b883fd51cb4355ae8da36427e
pinokara <hec.stw@gmail.com>
2019-07-19 08:06:29 +0700

    Change locale stype to global

--- global/router/userRouter.js
+++ global/router/userRouter.js
@@ -8,41 +8,42 @@ const settings = require('../../initial/settings');
 const fs = require('fs');
 const path = require('path');
 const indexPath = fs.realpathSync(process.cwd());
+const setupMongoDb = require('../../services/mongo-setup');
 
 module.exports = (router)=>{
-    // router.post('/users/register', async (req, res) => {
-    //     User.findOne({
-    //         email: req.body.email
-    //     }).then(user => {
-    //         if(user) {
-    //             return res.status(400).json({
-    //                 email: 'Email already exists'
-    //             });
-    //         }
-    //         else {
-    //             bcrypt.genSalt(10, (err, salt) => {
-    //                 if(err) console.error('There was an error', err);
-    //                 else {
-    //                     bcrypt.hash(req.body.password, salt, (err, hash) => {
-    //                         if(err) console.error('There was an error', err);
-    //                         else {
-    //                             const newUser = new User({
-    //                                 _id: new mongoose.Types.ObjectId,
-    //                                 email: req.body.email,
-    //                                 password: hash,
-    //                             });
-    //                             newUser
-    //                                 .save()
-    //                                 .then(user => {
-    //                                     res.json(user)
-    //                                 });
-    //                         }
-    //                     });
-    //                 }
-    //             });
-    //         }
-    //     });
-    // });
+    router.post('/users/register', async (req, res) => {
+        User.findOne({
+            email: req.body.email
+        }).then(user => {
+            if(user) {
+                return res.status(400).json({
+                    email: 'Email already exists'
+                });
+            }
+            else {
+                bcrypt.genSalt(10, (err, salt) => {
+                    if(err) console.error('There was an error', err);
+                    else {
+                        bcrypt.hash(req.body.password, salt, (err, hash) => {
+                            if(err) console.error('There was an error', err);
+                            else {
+                                const newUser = new User({
+                                    _id: new mongoose.Types.ObjectId,
+                                    email: req.body.email,
+                                    password: hash,
+                                });
+                                newUser
+                                    .save()
+                                    .then(user => {
+                                        res.json(user)
+                                    });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
 
     router.post('/users/login', (req, res) => {
         const email = req.body.email;
@@ -61,13 +62,13 @@ module.exports = (router)=>{
                                 email: user.email,
                             };
                             jwt.sign(payload, 'pinokarahere', {
-                                expiresIn: 3600
+                                expiresIn: 36000
                             }, (err, token) => {
                                 if(err) console.error('There is some error in token', err);
                                 else {
                                     res.json({
                                         success: true,
-                                        token: `Bearer ${token}`
+                                        token: `${settings.vpsIndex} ${token}`
                                     });
                                 }
                             });
@@ -80,10 +81,11 @@ module.exports = (router)=>{
             });
     });
 
-    router.post('/users/insert_coin', passport.authenticate('jwt', { session: false }), async (req, res) => {
-        let id = req.body.id;
-        let check = await List.findOne({coinId: id});
+    router.post('/users/insert_coin', passport.authenticate('jwt', { session: false }),  async (req, res) => {
+        let coinId = req.body.coinId;
+        let check = await List.findOne({coinId: coinId});
         if (check === null) {
+            await setupMongoDb(coinId);
             let wallet = {
                 host : 'localhost',
                 port: parseInt(req.body.port),
@@ -91,24 +93,25 @@ module.exports = (router)=>{
                 pass: settings.dbsettings.password,
                 timeout: 8000
             };
-            let base = {
-                name: req.body.name || id,
-                symbol: req.body.symbol || id,
-                category: req.body.category || 'masternode',
+            await List.create({
+                coinId,
+                name: req.body.name || coinTd,
                 active: req.body.active || true,
+                category: req.body.category || 'masternode',
+                symbol: req.body.symbol || coinId,
                 links: {
                     homepage: [req.body.homepage || null],
                     annoucement: [req.body.annoucement || null],
-                }
-            };
-            let data = JSON.stringify(Object.assign({id, wallet, base}));
-            let destination = path.resolve(indexPath, `initial/${id}.json`);
-            await fs.writeFileSync(destination, data);
-            await List.create({
-                coinId: id,
-                name: req.body.name || id,
-                active: req.body.active || true,
-                category: req.body.category || 'masternode'
+                    discord: [req.body.discord || null],
+                    twitter: [req.body.twitter || null],
+                    telegram: [req.body.telegram || null],
+                    facebook: [req.body.facebook || null],
+                    youtube: [req.body.youtube || null],
+                    explorer: [req.body.explorer || null],
+                    github: [req.body.github || null],
+                    reddit:[req.body.reddit || null],
+                },
+                wallet
             });
             res.status(401).json({status: 'success', message: 'uploaded'})
         } else {
