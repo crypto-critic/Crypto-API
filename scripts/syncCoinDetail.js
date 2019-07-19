e39fd0cf5b46a0869a903e120440ddf47624853c
pinokara <hec.stw@gmail.com>
2019-07-19 08:07:27 +0700

    Rename as profesional

---
+++ scripts/syncCoinDetail.js
@@ -0,0 +1,68 @@
+require('babel-polyfill');
+const exit = require('../lib/exit');
+const fetch = require('../lib/fetch');
+const locker = require('../lib/locker');
+const moment = require('moment');
+const getList = require(`../global/getList`);
+const CronJob = require('cron').CronJob;
+
+const everyMintute = '0 * * * * *' // Job every minute
+
+var coin = async (coin) => {
+    const Rich = await coin.rich;
+    const Coin = await coin.coin;
+    const rpc = await coin.rpc;
+    async function syncCoin() {
+        const date = moment().utc().startOf('minute').toDate();
+        const info = await rpc.call('getinfo');
+        const masternodes = await rpc.call('getmasternodecount');
+        const nethashps = await rpc.call('getnetworkhashps');
+        var activewallets = 0;
+        await Rich.find({ 'value': { $gt: 0 } }).count(function(err, count) {
+            if (err) { console.log(err) }
+            activewallets = count;
+        });
+
+        const coinx = new Coin({
+            createdAt: date,
+            blocks: info.blocks,
+            diff: info.difficulty,
+            mnsOff: masternodes.total - masternodes.stable,
+            mnsOn: masternodes.stable,
+            netHash: nethashps,
+            peers: info.connections,
+            status: 'Online',
+            supply: info.moneysupply,
+        });
+        await coinx.save();
+    }
+    async function update() {
+        const type = 'coin';
+        let code = 0;
+
+        try {
+            locker.lock(coin.coinId, type);
+            await syncCoin();
+        } catch (err) {
+            console.log(err);
+            code = 1;
+        } finally {
+            try {
+                locker.unlock(coin.coinId, type);
+            } catch (err) {
+                console.log(err);
+                code = 1;
+            }
+            // exit(code);
+        }
+    }
+    update();
+};
+
+const syncCoinDetail = () => getList().then(data => {
+    data.map(i => {
+        coin(i);
+    });
+});
+
+module.exports = syncCoinDetail;
---
+++ scripts/syncMasternodeList.js
@@ -0,0 +1,65 @@
+require('babel-polyfill');
+const exit = require('../lib/exit');
+const fetch = require('../lib/fetch');
+const {forEach} = require('p-iteration');
+const locker = require('../lib/locker');
+const moment = require('moment');
+const getList = require(`../global/getList`);
+// Models.
+var masternode = async (coin) => {
+    const Masternode = await coin.masternode;
+    const rpc = await coin.rpc;
+    async function syncMasternode() {
+        const date = moment().utc().startOf('minute').toDate();
+        await Masternode.deleteMany({});
+        rpc.timeout(10000); // 10 secs
+        const mns = await rpc.call('masternode', ['list']);
+        const inserts = [];
+        await forEach(mns, async (mn) => {
+            const masternode = new Masternode({
+                active: mn.activetime,
+                addr: mn.addr,
+                createdAt: date,
+                lastAt: new Date(mn.lastseen * 1000),
+                lastPaidAt: new Date(mn.lastpaid * 1000),
+                network: mn.network,
+                rank: mn.rank,
+                status: mn.status,
+                txHash: mn.txhash,
+                txOutIdx: mn.outidx,
+                ver: mn.version
+            });
+            inserts.push(masternode);
+        });
+        if (inserts.length) {
+            await Masternode.insertMany(inserts);
+        }
+    }
+    async function update() {
+        const type = 'masternode';
+        let code = 0;
+        try {
+            locker.lock(coin.coinId, type);
+            await syncMasternode();
+        } catch(err) {
+            console.log(err);
+            code = 1;
+        } finally {
+            try {
+                locker.unlock(coin.coinId, type);
+            } catch(err) {
+                console.log(err);
+                code = 1;
+            }
+            // exit(code);
+        }
+    }
+    update();
+};
+const syncMasternodeDetail = () => getList().then(data => {
+    data.map(i => {
+        masternode(i);
+    });
+});
+
+module.exports = syncMasternodeDetail;
