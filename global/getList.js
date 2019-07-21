d3a28c8733b51619d95aac8547e971aa3e9ce162
pinokara <hec.stw@gmail.com>
2019-07-21 12:28:03 +0700

    Update active coin

--- global/getList.js
+++ global/getList.js
@@ -2,7 +2,7 @@ const List = require('./models/ListCoin');
 const Coin = require('../coin/coin');
 
 const getList = async () => {
-    data = await List.find({});
+    data = await List.find({active: true});
     let ListCoin = await data.map(data => new Coin(data));
     return ListCoin;
 }
