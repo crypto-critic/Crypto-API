6d9bcb6da8d33982e878be4fe17171910a955902
pinokara <hec.stw@gmail.com>
2019-07-21 22:05:54 +0700

    Add pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,17 @@
+const pm2Config = {
+    "apps": [
+      {
+        "name": "app",
+        "script": "./server/index.js",
+        "exec_mode": "cluster_mode",
+        "instances": "max"
+      },
+      {
+        "name": "cronJob",
+        "script": "./server/cronJobs.js",
+        "instances": 2
+      }
+    ]
+};
+
+module.exports = pm2Config;
