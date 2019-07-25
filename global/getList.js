2b699eeb86752995af2f6ceca0b27eb3fca24850
pinokara <hec.stw@gmail.com>
2019-07-25 16:42:46 +0700

    update

--- global/getList.js
+++ global/getList.js
@@ -4,7 +4,7 @@ const Coin = require('../coin/coin');
 const getList = async () => {
     data = await List.find({active: true});
     console.log(data);
-    // let ListCoin = await data.map(data => new Coin(data));
+    let ListCoin = await data.map(data => new Coin(data));
     return ListCoin;
 }
 module.exports = getList;
