2996d88c517c96990407c463646b31809a5f7d0d
pinokara <hec.stw@gmail.com>
2019-07-26 00:33:37 +0700

    {new: false} at mongo v3

--- scripts/syncMarketData.js
+++ scripts/syncMarketData.js
@@ -8,14 +8,14 @@ const syncMarket =  async (coin) => {
     const UTXO = coin.utxo;
     const Coin = coin.coin;
     const Rpc = coin.rpc;
-    const chain = await require(`../initial/${coin.coinId}.chain`);
+    const chain = await require(`../initial/${coin.coinId}.chain.js`);
 
     let coinCollection = await Coin.findOne().sort({ createdAt: -1 });
     let coinId = coin.coinId;
     let marketId = coin.marketId;
     let data = await getDataFromCoingecko(coin.marketId);
     let market_data = data.market_data;
-    let info = Rpc.call('getinfo');
+    let info = await Rpc.call('getinfo');
     let nHeight = info.blocks;
     let nMasternodeCount = coinCollection.mnsOff + coinCollection.mnsOn;
     let nMoneySupply = await cache.getFromCache("supply", moment().utc().add(1, 'hours').unix(), async() => {
@@ -24,24 +24,24 @@ const syncMarket =  async (coin) => {
             ]);
             t = utxo[0].total;
             return t;
-        });
+    });
     let nSubsidy = chain.getSubsidy(nHeight);
     let mnratio = chain.getMNSubsidy(nHeight, nMasternodeCount, nMoneySupply);
-    let colateral = chain.colateral;
-    let ROI = chain.getMNBlocksPerYear(nMasternodeCount)*nSubsidy*mnratio/colateral*100 || 0;
+    let collateral = chain.collateral;
+    let ROI = chain.getMNBlocksPerYear(nMasternodeCount)*nSubsidy*mnratio/collateral*100 || 0;
     let coinPerDay = chain.getMNBlocksPerDay(nMasternodeCount)*nSubsidy*mnratio || 0;
     let dailyIncome = {coin: coinPerDay};
     let weeklyIncome = {coin: coinPerDay*7};
     let monthlyIncome = {coin: coinPerDay*30};
     let yearlyIncome = {coin: coinPerDay*365.25};
-    let masternodeWorth = {coin: colateral};
+    let masternodeWorth = {coin: collateral};
     let market_cap = {}
     await Object.keys(market_data.current_price).forEach(key => {
         dailyIncome[key] = market_data.current_price[key]*coinPerDay;
         weeklyIncome[key] = market_data.current_price[key]*coinPerDay*7;
         monthlyIncome[key] = market_data.current_price[key]*coinPerDay*30;
         yearlyIncome[key] = market_data.current_price[key]*coinPerDay*365.25;
-        masternodeWorth[key] = market_data.current_price[key]*colateral;
+        masternodeWorth[key] = market_data.current_price[key]*collateral;
         market_cap[key] = market_data.current_price[key]*nMoneySupply;
     });
     market_data = {...market_data, market_cap}
@@ -53,8 +53,8 @@ const syncMarket =  async (coin) => {
             income_data,
             last_updated: data.last_updated
         }},
-        {upsert: true, new: true},
-        () => console.log('finish')
+        {upsert: true},
+        () => console.log('Sync marketdata')
     )
 };
 const syncMarketData = () => getList().then(data => {
@@ -62,6 +62,5 @@ const syncMarketData = () => getList().then(data => {
         syncMarket(i);
     })};
 });
-syncMarketData();
 
 module.exports = syncMarketData;
